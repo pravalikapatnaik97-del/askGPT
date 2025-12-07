3d5a4354e1d82dfc1baf23117b937f29a656362e
pravalikapatnaik97-del <pravalikapatnaik97@gmail.com>
2025-12-07 16:25:13 +0530

    Add files via upload

---
+++ Code.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+from google import genai
+
+client = genai.Client(api_key="AIzaSyB6IaZFcW2c3YXQwvRH5vy10zRzRBAw0ic")
+st.title("askGPT")
+def inputs(prompt):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=f"Please summerise the file: {prompt}",
+    )
+    return response.text
+file=st.file_uploader("Upload a file for summerizing")
+if st.button('Upload'):
+    if file:
+        with st.spinner("Reading the file..."): 
+            prompt=file.read()
+            result=inputs(prompt)      
+            st.write(result)
+            st.snow()
+    else:
+        st.warning("1st ask any question!")
+
